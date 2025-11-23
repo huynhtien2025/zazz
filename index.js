dac7c2ac520fa74b1852d52d37b7b4cf23180728
Linh <huynhtien.2024tien@gmail.com>
2025-11-23 13:31:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import cfonts from 'cfonts';
       letterSpacing: 1,
       lineHeight: 1,
       space: true,bvbvbvbxssf
-          fhhrdfdvgffvcvdvvgfggfggf
+          fhhrdfdvgffvcvdvvgfggfggfdfdfdffd
       maxLength: '0',scssvsvsvcvcvcvddffggf
     });cvcvfdbbbvcvcvcv
     console.log(chalk.green("===dfdf ==="));
@@ -98,6 +98,7 @@ const deleteMessage = async (channelId, messageId, token) => {
         }
     }
 })();
+
 
 
 
