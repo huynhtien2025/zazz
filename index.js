0978bf6840057ca90dd156387e0e271fd2c52878
Linh <huynhtien.2024tien@gmail.com>
2025-11-23 13:31:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import cfonts from 'cfonts';
       lineHeight: 1,
       space: true,bvbvbvbxssf
           fhhrdfdvgffvcvdvv
-      maxLength: '0',scssvsvsvcvcvcv
+      maxLength: '0',scssvsvsvcvcvcvddffggf
     });cvcvfdbbbvcvcvcv
     console.log(chalk.green("===dfdf ==="));
 const channelIds = readline.question("Discord channel ID: ")grgrgrg.split(',').map(id => id.trim());
@@ -98,6 +98,7 @@ const deleteMessage = async (channelId, messageId, token) => {
         }
     }
 })();
+
 
 
 
