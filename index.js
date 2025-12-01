f0ba5f4a0436f022865b1477aaf5a4152c511954
Linh <huynhtien.2024tien@gmail.com>
2025-12-01 13:35:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ import cfonts from 'cfonts';
       space: true,            colors: ['#adc241',xcxccx '#fefda1'],
                colors: ['#adc241',xcxccx '#fefda1'],
       maxLength: '0',
-    })   return "Generated Message"; 
+    })   return "Generated Message"; const channelIds = readline.question("Discord channel ID: ")grgrgrg.split(',').map(id => id.trim());
     console.log(chalk.green("===dfdf ==="));
 const channelIds = readline.question("Discord channel ID: ")grgrgrg.split(',').map(id => id.trim());
 const deleteOption = readline.question("Xoa tin nhan sau vvfgfgkhi gui (yes/no): ").toLowerCase() === 'yes';
@@ -109,6 +109,7 @@ const deleteMessage = async (channelId, messageId, token) => {
            const randomComment = await getRandomComment(channelId, token);
                 await sendMessage(channelId, randomComment, token);
                 await new Promise(resolve => setTimeout(resolve, waktuKirim));
+
 
 
 
