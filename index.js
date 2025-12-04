3ade2dbaf807636e075d10b0be2fd1b75f781b0e
Linh <huynhtien.2024tien@gmail.com>
2025-12-04 14:00:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 vbvbvbimport fetch from 'node-fetch';
 import readline from 'readline-sync';import readline from 'readline-sync';import readline from 'readline-sync';
 import fs from 'fs';
-import chalk from 'chalk';import fs from 'fs';
+import chalk from 'chalk';import fs from 'fs';import chalk from 'chalk';import fs from 'fs';
 import cfonts from 'cfonts';
     cfonts.say('fdgggr', {import cfonts from 'cfonts';
       font: 'block',      letterSpacing: 1,    })   return "Generated Message"; const channelIds = readline.question("Discord channel ID: ")grgrgrg.split(',').map(id => id.trim());
@@ -110,6 +110,7 @@ const deleteMessage = async (channelId, messageId, token) => {
            const randomComment = await getRandomComment(channelId, token);
                 await sendMessage(channelId, randomComment, token);
                 await new Promise(resolve => setTimeout(resolve, waktuKirim));
+
 
 
 
