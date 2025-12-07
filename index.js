c530ec4610c82edddffc6aade0c12a75004b9fd1
Linh <huynhtien.2024tien@gmail.com>
2025-12-07 12:27:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 vbvbvbimport fetch from 'node-fetch';import cfonts from 'cfonts';import chalk from 'chalk';import fs from 'fs';vbvbvbimport fetch from 'node-fetch';import cfonts from 'cfonts';import chalk from 'chalk';import fs from 'fs';
 import readline from 'readline-sync';import readline from 'readline-sync';import readline from 'readline-sync';
-import fs from 'fs'; import fs from 'fs';Join soso value
+import fs from 'fs'; import fs from 'fs';
 import chalk from 'chalk';import fs from 'fs';import chalk from 'chalk';import fs from 'fs';
 import cfonts from 'cfonts';import chalk from 'chalk';import fs from 'fs';import cfonts from 'cfonts';import chalk from 'chalk';import fs from 'fs';
     cfonts.say('fdgggr', {import cfonts from 'cfonts';
@@ -110,6 +110,7 @@ const deleteMessage = async (channelId, messageId, token) => {
            const randomComment = await getRandomComment(channelId, token);
                 await sendMessage(channelId, randomComment, token);
                 await new Promise(resolve => setTimeout(resolve, waktuKirim));
+
 
 
 
